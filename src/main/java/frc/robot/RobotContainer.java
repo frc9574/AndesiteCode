52d5ab8c2fd67c965df0724f0ecac6176d4a3a42
userj <47265397+UserJHansen@users.noreply.github.com>
2024-01-11 13:03:32 +1000

    Yipppeee advantagekit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,61 +1,114 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// Copyright 2021-2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.TankSubsystem;
-
-import com.revrobotics.CANSparkBase.IdleMode;
-
-import edu.wpi.first.cameraserver.CameraServer;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.FeedForwardCharacterization;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.DriveIO;
+import frc.robot.subsystems.drive.DriveIOSim;
+import frc.robot.subsystems.drive.DriveIOSparkMax;
+import frc.robot.subsystems.launcher.Launcher;
+import frc.robot.subsystems.launcher.LauncherIO;
+import frc.robot.subsystems.launcher.LauncherIOSim;
+import frc.robot.subsystems.launcher.LauncherIOSparkMax;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
+ * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  // Subsystems
+  private final Drive drive;
+  private final Launcher launcher;
 
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  // Controller
+  private final CommandXboxController controller = new CommandXboxController(0);
 
-  private final TankSubsystem m_drivebase = new TankSubsystem(m_driverController, IdleMode.kCoast);
+  // Dashboard inputs
+  private final LoggedDashboardChooser<Command> autoChooser;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
+    switch (Constants.currentMode) {
+      case REAL:
+        // Real robot, instantiate hardware IO implementations
+        // drive = new Drive(new DriveIOSparkMaxBrushed()); // Spark Max/Spark Flex + brushed, no
+        // encoders
+        drive = new Drive(new DriveIOSparkMax()); // Spark Max/Spark Flex + NEO/Vortex
 
-    CameraServer.startAutomaticCapture();
+        // launcher = new Launcher(new LauncherIOSparkMaxBrushed()); // Spark Max/Spark Flex +
+        // brushed, no encoders
+        launcher = new Launcher(new LauncherIOSparkMax()); // Spark Max/Spark Flex + NEO/Vortex
+        break;
+
+      case SIM:
+        // Sim robot, instantiate physics sim IO implementations
+        drive = new Drive(new DriveIOSim());
+        launcher = new Launcher(new LauncherIOSim());
+        break;
+
+      default:
+        // Replayed robot, disable IO implementations
+        drive = new Drive(new DriveIO() {});
+        launcher = new Launcher(new LauncherIO() {});
+        break;
+    }
+
+    // Set up auto routines
+    NamedCommands.registerCommand("Launch", launcher.launchCommand().withTimeout(3.0));
+    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+    autoChooser.addOption(
+        "Launch & Leave (No Encoders)",
+        launcher
+            .launchCommand()
+            .withTimeout(3.0)
+            .andThen(
+                Commands.startEnd(() -> drive.driveVolts(-3.0, -3.0), drive::stop, drive)
+                    .withTimeout(3.0)));
+
+    // Set up feedforward characterization
+    autoChooser.addOption(
+        "Drive FF Characterization",
+        new FeedForwardCharacterization(
+            drive, (volts) -> drive.driveVolts(volts, volts), drive::getCharacterizationVelocity));
+
+    // Configure the button bindings
+    configureButtonBindings();
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  private void configureButtonBindings() {
+    drive.setDefaultCommand(
+        Commands.run(
+            () -> drive.driveArcade(-controller.getLeftY(), controller.getLeftX()), drive));
+    controller.leftBumper().whileTrue(launcher.intakeCommand());
+    controller.rightBumper().whileTrue(launcher.launchCommand());
   }
 
   /**
@@ -64,15 +117,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
-  }
-
-  public TankSubsystem getDrivebase() {
-    return m_drivebase;
-  }
-
-  public void initSimulation() {
-    m_drivebase.initSimulation();
+    return autoChooser.get();
   }
 }
